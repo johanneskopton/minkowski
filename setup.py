2efdd3a38a82b7dcaac42e3fd2617e2b62a67bb0
Johannes Kopton <johannes@kopton.org>
2025-02-12 16:36:32 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="stif",
-    version="1.0.1",
+    version="1.0.2",
     author="Johannes Kopton",
     author_email="johannes@kopton.org",
     description=("Space-Time Interpolation and Forecasting"),
